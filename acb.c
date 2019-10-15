7af29cede34dc3ec422aa6a192da570a6a80d4fa
wyb-byw <2843043708@qq.com>
2019-10-15 09:43:22 +0800

    ace

---
+++ acb.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (void)
+{
+    int length,width,height,v;
+    length = 10;
+    width = 5;
+    height = 6;
+    v = length * width * height;
+    printf("v is %d!\n", v);
+    return 0;
+}
